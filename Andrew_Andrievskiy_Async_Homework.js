9638ad1454153644d6ab0d40972e4935365c540b
Freemail207 <freemail207@gmail.com>
2016-11-17 22:32:53 +0200

    Homework

---
+++ Andrew_Andrievskiy_Async_Homework.js
@@ -0,0 +1,56 @@
+/**
+ * Created by user on 17.11.2016.
+ */
+function getBookById(id) {
+    document.getElementById('book').textContent = 'Please wait. Book is loading';
+
+    fetch('api/books/' + id,
+        {
+            method: 'GET'
+        })
+        .then(function (response) {
+            document.getElementById('book').textContent = response.name;
+        })
+        .catch(function (error) {
+            document.getElementById('book').textContent = 'Error. Please refresh your browser';
+        });
+}
+/////////////////
+function loadPage(bookId) {
+    document.getElementById('book').textContent = 'Please wait. Book is loading';
+    document.getElementById('author').textContent = 'Please wait. Author details are loading';
+    document.getElementById('similar').textContent = 'Please wait. Similar books are loading';
+
+    fetch('api/books/' + id, {
+        method: 'GET'
+    }).
+        then(function (response) {
+            document.getElementById('book').textContent = response.name;
+            fetch('api/autors' + response.authorId,
+                {
+                    method: 'GET'
+                }).then(function (response) {
+                    document.getElementById('author').textContent = response.name;
+                    var similarBooksLoaded = 0;
+                    var similarBooksAmount = response.books.lenght;
+                    Promise.all(response.books.forEach(function (similarBookId) {
+                        fetch('api/bestsellers/similar/' + similarBookId, {
+                            method: 'GET'
+                        }).then(function (response) {
+                            var p = document.getElementById('similar').appendChild('p').textContent = response;
+                            similarBooksLoaded += 1
+                            if (similarBooksLoaded === similarBooksAmount) {
+                                alert('Horray everything loaded');
+                            }
+
+                        });
+                        return response;
+                    }));
+                    return response;
+                });
+        })
+        .catch(function (error) {
+            document.getElementById('book').textContent = 'Error. Please refresh your browser';
+        });
+
+}
